e5f92e9b11b31996257c0d37569ff2a005a91f6d
imsonugupta01 <kumarsonu94047@gmail.com>
2025-05-07 02:26:00 +0530

    till login & poster

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/upload', uploadRoutes);
 
 
+app.get("/", (req, res) => {
+    res.send("Live");
+  });
 // Start server
 app.listen(config.port, () => {
   console.log(`Server running on http://localhost:${config.port}`);
